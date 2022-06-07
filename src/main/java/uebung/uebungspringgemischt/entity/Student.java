effe01729d90fe6999f998e9057fdca1ccd8fac2
David Ebner <2010653205@stud.fh-kufstein.ac.at>
2022-06-07 15:59:06 +0200

    updated Student entity
    - added password String
    - added authorities String array

--- src/main/java/uebung/uebungspringgemischt/entity/Student.java
+++ src/main/java/uebung/uebungspringgemischt/entity/Student.java
@@ -1,15 +1,21 @@
 package uebung.uebungspringgemischt.entity;
 
 import java.util.List;
+import java.util.Set;
 
 public class Student extends Person {
     private String matriculationNumber; // PKZ
+    private String password;
+    private Set<String> authorities;
     private String startYear; // Jahrgang
     private List<Semester> semesters;
 
-    public Student(int id, String firstName, String lastName, String matriculationNumber, String startYear) {
+    public Student(int id, String firstName, String lastName, String matriculationNumber,
+                   String password, Set<String> authorities, String startYear) {
         super(id, firstName, lastName);
         this.matriculationNumber = matriculationNumber;
+        this.password = password;
+        this.authorities = authorities;
         this.startYear = startYear;
     }
 
@@ -23,6 +29,22 @@ public class Student extends Person {
 
     public void setMatriculationNumber(String matriculationNumber) {
         this.matriculationNumber = matriculationNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<String> authorities) {
+        this.authorities = authorities;
     }
 
     public String getStartYear() {
